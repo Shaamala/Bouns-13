7d3874d69e1a8ca8961e480dcc7450a33f7fd48a
Shaamala <shaamalajabbar@gmail.com>
2018-02-18 18:08:18 -0500

    First commit

---
+++ script.js
@@ -0,0 +1,7 @@
+// 1. Create the button
+var button = document.createElement("button");
+button.innerHTML = "Country Visited";
+
+// 2. Append somewhere
+var body = document.getElementsByTagName("body")[0];
+body.appendChild(button);
